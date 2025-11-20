b0ccbe59b9ad2094dde43704fa77847aea09a757
Vijay Netekal <vijaynetekal28@gmail.com>
2025-11-21 03:50:04 +0530

    Auto-create superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# --- THE TRICK ---
+# This python command checks if 'admin' exists. If NOT, it creates one with password 'admin123'
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@campus.com', 'admin123')"
+
+
